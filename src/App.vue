876dd48a527e10e857d08484c6bb305a13b1cbd3
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-12-14 21:53:38 +0900

    カメラ画像読み取り

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+const video = ref<HTMLVideoElement>();
+
+onMounted(async () => {
+  if (!video.value) return;
+
+  video.value.srcObject = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: { facingMode: "environment" },
+  });
+});
+</script>
+
+<template>
+  <video ref="video" width="360" height="240" autoplay />
+</template>
+
+<style scoped>
+video {
+  background-color: #eee;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,21 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { certificateFor } from "devcert";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(async () => {
+  const { key, cert } = await certificateFor("localhost");
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    server: {
+      https: { key, cert },
+    },
+  };
 });
