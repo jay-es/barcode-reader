084ba7985a64dc6f72d74c37a5d8611c1fe6cd75
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-12-22 23:34:38 +0900

    スタイル整理

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,16 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-
-const video = ref<HTMLVideoElement>();
-const barcode = ref("");
-
-onMounted(async () => {
-  if (!video.value) return;
-
-  video.value.srcObject = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      facingMode: "environment", // 背面カメラ
-    },
-  });
-});
-
-setInterval(async () => {
-  if (!video.value || barcode.value) return;
-
-  const detector = new (window as any).BarcodeDetector({ formats: ["ean_13"] });
-  const result = await detector.detect(video.value);
-
-  if (result?.[0]?.rawValue) {
-    barcode.value = result[0].rawValue;
-    video.value.pause();
-  }
-}, 1000);
-
-const reset = () => {
-  barcode.value = "";
-  video.value?.play();
-};
+import BarcodeReader from "./components/BarcodeReader.vue";
 </script>
 
 <template>
-  barcode: {{ barcode }}<br />
-  <video ref="video" width="240" autoplay />
-  <button @click="reset()">reset</button>
+  <header class="container"><h1>Barcode Reader</h1></header>
+  <main class="container">
+    <BarcodeReader />
+  </main>
 </template>
 
 <style scoped>
-video {
-  display: block;
+h1 {
+  margin-bottom: 0.5rem;
 }
 </style>
---
+++ src/components/BarcodeReader.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const video = ref<HTMLVideoElement>();
+const barcode = ref("");
+
+onMounted(async () => {
+  if (!video.value) return;
+
+  video.value.srcObject = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: "environment", // 背面カメラ
+    },
+  });
+});
+
+setInterval(async () => {
+  if (!video.value || barcode.value) return;
+
+  const detector = new (window as any).BarcodeDetector({ formats: ["ean_13"] });
+  const result = await detector.detect(video.value);
+
+  if (result?.[0]?.rawValue) {
+    barcode.value = result[0].rawValue;
+    video.value.pause();
+  }
+}, 1000);
+
+const reset = () => {
+  barcode.value = "";
+  video.value?.play();
+};
+</script>
+
+<template>
+  <video ref="video" width="240" autoplay />
+  <template v-if="barcode">
+    <p>barcode: {{ barcode }}</p>
+    <button @click="reset()">reset</button>
+  </template>
+</template>
+
+<style scoped>
+video {
+  display: block;
+  margin: 0.75rem auto;
+}
+</style>
