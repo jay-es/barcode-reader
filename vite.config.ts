d868335a1eb04662cd9c6ca558aa3be0b125e73e
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-12-23 21:29:00 +0900

    SSL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { certificateFor } from "devcert";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
+export default defineConfig(async () => {
+  const { key, cert } = await certificateFor("localhost");
+
+  return {
+    plugins: [preact()],
+    server: {
+      https: { key, cert },
+    },
+  };
 });
