b7c3c90607cc198cc232f7232cf68950617f43d2
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-12-23 21:09:37 +0900

    vite init

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import "@picocss/pico/css/pico.min.css";
-
-createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { certificateFor } from "devcert";
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => {
-  const { key, cert } = await certificateFor("localhost");
-
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    server: {
-      https: { key, cert },
-    },
-  };
-});
+export default defineConfig({
+  plugins: [preact()],
+})
