54f495e09ce1aa2ce51ef068b39d5d405ab549e9
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-05-05 22:30:17 +0900

    testing-library/jest-dom 追加

--- src/components/BarcodeList/BarcodeList.spec.tsx
+++ src/components/BarcodeList/BarcodeList.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/preact";
+import { render, screen, within } from "@testing-library/preact";
 import { describe, expect, it } from "vitest";
 
 import { BarcodeList } from "./BarcodeList";
@@ -6,12 +6,18 @@ import { BarcodeList } from "./BarcodeList";
 describe("BarcodeList", () => {
   it("リストがなければ li はレンダリングされない", () => {
     render(<BarcodeList barcodes={[]} />);
-    expect(screen.queryAllByRole("listitem")).toHaveLength(0);
+    const list = screen.getByRole("list");
+
+    expect(list).toBeInTheDocument();
+    expect(within(list).queryAllByRole("listitem")).toHaveLength(0);
   });
 
   it("barcodes の数だけ li が表示される", () => {
     const barcodes = ["foo", "bar", "baz"];
     render(<BarcodeList barcodes={barcodes} />);
-    expect(screen.queryAllByRole("listitem")).toHaveLength(3);
+    const list = screen.getByRole("list");
+
+    expect(list).toBeInTheDocument();
+    expect(within(list).queryAllByRole("listitem")).toHaveLength(3);
   });
 });
